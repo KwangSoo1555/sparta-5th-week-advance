f295dd82bb11ade33c789eeeb759e4d3e769ddb0
Bok Kwang Soo <qhrrhkdtn1555@gmail.com>
2024-06-17 06:58:29 +0900

    feat/eslint and .env.example create

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: ['no-relative-import-paths'],
+    extends: [
+      '@rushstack/eslint-config/profile/web-app',
+    ],
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+    settings: {
+    },
+  };
